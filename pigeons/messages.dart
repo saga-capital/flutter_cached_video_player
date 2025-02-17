99cf690502d7fffce23162942f59de11c8e16c64
Hamidreza Bayat <hamidrezabayat76@gmail.com>
2025-02-17 19:23:07 +0330

    Update dependencies

--- pigeons/messages.dart
+++ pigeons/messages.dart
@@ -2,9 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// @dart = 2.12
-
-import 'package:pigeon/pigeon_lib.dart';
+import 'package:pigeon/pigeon.dart';
 
 class TextureMessage {
   int? textureId;
